705f94bef7fe1b90aa492f4076d32e65cf588823
Kishan Kr Sharma <skishan781@gmail.com>
2023-04-23 20:04:04 +0545

    api request created

--- RickAndMorty/ApiClient/RMService.swift
+++ RickAndMorty/ApiClient/RMService.swift
@@ -18,7 +18,11 @@ final class RMService {
     /// - Parameters:
     ///   - request: Request instance
     ///   - completion: Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping() -> Void) {
+    ///   - type: The type of object we expect to get back
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping(Result<T, Error>) -> Void) {
         
     }
 }
--- RickAndMorty/Models/Character/RMCharacter.swift
+++ RickAndMorty/Models/Character/RMCharacter.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct RMCharacters: Codable{
+struct RMCharacter: Codable{
     let id: Int
     let name: String
     let status: RMCharacterStatus
