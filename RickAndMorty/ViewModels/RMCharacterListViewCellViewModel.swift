b681f7d621383808bb35da26ac369f64bcc3f2c9
Kishan Kr Sharma <skishan781@gmail.com>
2023-05-21 21:06:00 +0545

    Previously loaded image cache

--- RickAndMorty/ViewModels/RMCharacterListViewCellViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterListViewCellViewModel.swift
@@ -33,16 +33,9 @@ final class RMCharacterListViewCellViewModel: Hashable, Equatable {
             completion(.failure(URLError(.badURL)))
             return
         }
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request){ data,_,error in
-            guard let data = data, error == nil else {
-                completion(.failure(error ?? URLError(.badServerResponse)))
-                return
-            }
-            
-            completion(.success(data))
-        }
-        task.resume()
+        
+            let request = URLRequest(url: url)
+        RMImageLoader.shared.downloadImage(url, completion: completion)
     }
     
     // MARK: - Hashable
