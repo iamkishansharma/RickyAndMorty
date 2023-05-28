01a99d40f44de76f97d4bb65bfb059afda547d8a
Kishan Kr Sharma <skishan781@gmail.com>
2023-05-28 22:01:28 +0545

    Character details view composition layout setup

--- RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 /// Controller to show into about single character
 final class RMCharacterDetailViewController: UIViewController {
-
+    
     private let viewModel: RMCharacterDetailViewViewModel
+    private let detailView: RMCharacterDetailView
+    
+    // MARK: - Init
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
         
     }
@@ -28,7 +32,59 @@ final class RMCharacterDetailViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+        addConstraints()
+//        No fetch needed because we already have character
+//        viewModel.fetchCharacterData()
+        
+        detailView.collectionView?.delegate = self
+        detailView.collectionView?.dataSource = self
+    }
+    
+    
+    
+    @objc
+    private func didTapShare() {
+        // Share character's info
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
 
+}
+
+// MARK: - CollectionView
+
+extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.sections.count
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemPink
+        }else if indexPath.section == 1 {
+            cell.backgroundColor = .systemGreen
+        }else {
+            cell.backgroundColor = .systemBlue
+        }
+        return cell
+    }
 }
--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -9,11 +9,43 @@ import Foundation
 
 final class RMCharacterDetailViewViewModel {
     private let character: RMCharacter
+    
+    enum SectionType: CaseIterable {
+        case photo
+        case information
+        case episodes
+    }
+    
+    public let sections = SectionType.allCases
+    
+    // MARK: - Init
+    
     init (character: RMCharacter) {
         self.character = character
+    }
+    
+    private var requestUrl: URL? {
+        return URL(string: character.url)
     }
     
     public var title: String {
         character.name.capitalized
     }
+    
+//    public func fetchCharacterData() {
+//        guard let url = requestUrl,
+//              let request = RMRequest(url: url) else {
+//            return
+//        }
+//
+//        RMService.shared.execute(request, expecting: RMCharacter.self){ result in
+//            switch result {
+//            case .success(let success):
+//                print(String(describing: success))
+//            case .failure(let failure):
+//                print(String(describing: failure))
+//            }
+//        }
+//
+//    }
 }
